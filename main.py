51ba0895330f0231d4a428d3fee6c5145b68b3cf
Manuel Luna <manuel.luna@amd.com>
2024-12-03 11:21:04 -0300

    some test

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-## some comment
 
 import threading
 import webbrowser
--- main2.h
+++ main2.h
@@ -1,3 +1,2 @@
 // Copyright(C) 2024 Advanced Micro Devices, Inc. All rights reserved
 void function3() {}
-__cpp_deduction_guides
