63f00e6597eaa92461b15c11b03157a620b4a5e1
Manuel Luna <manuel.luna@amd.com>
2024-12-03 11:00:04 -0300

    test precommit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 // Copyright(C) 2024 Advanced Micro Devices, Inc. All rights reserved
-// Copyright (C) 2021 - 2023 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2020-2024 Advanced Micro Devices, Inc. All rights reserved.
 
 void function3() {}
-dd'
