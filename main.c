93e4bad235a1d12b7aa80dde3e5f857d32a11cd5
Manuel Luna <141169354+manuelqubika@users.noreply.github.com>
2024-12-17 14:42:01 -0300

    Test Formatter (#9)
    * some text
    
    * change action pr filter
    
    * test on call run
    
    * fix typo
    
    * pass branches names directly
    
    * add function
    
    * test token
    
    * test PAT
    
    * test runner
    
    * add some errors
    
    ---------
    
    Co-authored-by: Manuel Luna <manuel.luna@amd.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- main.c
+++ main.c
@@ -1,4 +1,41 @@
-// Copyright(C) 2024 Advanced Micro Devices, Inc. All rights reserved.
+#include <stdio.h>
 
-void function2() {}
+/* 
+ * Placeholder C Program
+ * Author: Your Name
+ * Date: YYYY-MM-DD
+ * Description: This is a basic C program template with a simple structure.
+ */
 
+#define VERSION "1.0.0"
+
+// Function Prototypes
+void placeholderFunction();
+
+int main(int argc, char *argv[]) {
+    // Basic Program Initialization
+      printf("Placeholder C Program Version %s\n", VERSION);
+printf("Number of arguments: %d\n", argc);
+
+    // Process command-line arguments
+    if (argc > 1) {
+        printf("Arguments received:\n");
+        for (int i = 0; i < argc; i++) {
+            printf("  argv[%d]: %s\n", i, argv[i]);
+        }
+    } else {
+        printf("No arguments provided.\n");
+    }
+
+    // Call a placeholder function
+    placeholderFunction();
+
+    // Exit program
+    printf("Program execution completed.\n");
+    return 0;
+}
+
+// Definition of placeholderFunction
+void placeholderFunction() {
+    printf("This is a placeholder function. Add your functionality here.\n");
+}
